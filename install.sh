b0977e72d9f522f19eb4cc2092f3f24ce6191188
zamygit <zamy.git@gmail.com>
2025-11-30 11:52:10 -0400

    actualizo install.sh

--- install.sh
+++ install.sh
@@ -5,14 +5,14 @@ sudo -v
 while true; do sudo -v; sleep 60; done &
 
 sudo pacman -Syu --noconfirm
-
-sudo pacman -S --noconfirm  rofi alacritty picom udiskie  feh python-psutil lxappearance lightdm-gtk-greeter-settings  breeze base-devel rust nitrogen vlc unzip  nemo firefox obsidian  git polybar
+sudo pacman -S --noconfirm  rofi alacritty picom udiskie  feh python-psutil lxappearance lightdm-gtk-greeter-settings 
+sudo pacman -S --noconfirm  breeze base-devel rust nitrogen vlc unzip  nemo firefox obsidian  git polybar 
 
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-paru -S papirus-icon-theme nerd-fonts arc-gtk-theme sublime-text-4 materia-gtk-theme neofetch   --noconfirm 
+paru -S papirus-icon-theme nerd-fonts arc-gtk-theme sublime-text-4 materia-gtk-theme neofetch arandr s  --noconfirm 
 
 git clone https://github.com/adi1090x/rofi.git
 

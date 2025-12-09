948d64e392a8bf34a60a36d61115138d7e59b504
zamygit <zamy.git@gmail.com>
2025-12-08 20:19:50 -0400

    cambio 20

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+!/bin/bash
 #pide el root solo una vez 
 sudo -v
 
@@ -6,15 +6,18 @@ while true; do sudo -v; sleep 60; done &
 
 sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm  rofi alacritty picom udiskie  feh python-psutil lxappearance lightdm-gtk-greeter-settings 
-sudo pacman -S --noconfirm  breeze base-devel rust nitrogen vlc unzip  nemo firefox obsidian  git polybar 
+sudo pacman -S --noconfirm  breeze base-devel rust vlc unzip  nemo firefox obsidian  git polybar 
 
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-paru -S papirus-icon-theme nerd-fonts arc-gtk-theme sublime-text-4 materia-gtk-theme neofetch arandr s  --noconfirm 
+paru -S papirus-icon-theme nerd-fonts arc-gtk-theme sublime-text-4 materia-gtk-theme neofetch arandr   --noconfirm 
 
 git clone https://github.com/adi1090x/rofi.git
+cd paru/rofi
+./setup.sh
+
 
 curl -O https://blackarch.org/strap.sh
 chmod +x strap.sh
